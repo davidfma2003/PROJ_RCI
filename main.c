54d702c2fe3e94158015b5d58060b6ab736bc44c
Velo123 <miguelsvelo@gmail.com>
2024-02-26 15:12:12 +0000

    Added main.h

--- main.c
+++ main.c
@@ -1,1 +1,15 @@
-#include stdin
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "main.h"
+
+int main(int argc, char *argv[]){
+
+
+
+
+
+    
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+// Function declarations or prototypes
+
+int main(int argc, char *argv[]);
+
+#endif // MAIN_H
