c5057af82def31637902be24966cf3b644f9da89
davidfma2003 <davidfma2003@gmail.com>
2024-02-27 09:11:35 +0000

    dia1

--- main.c
+++ main.c
@@ -5,7 +5,11 @@
 #include "main.h"
 
 int main(int argc, char *argv[]){
-
+    char reg_IP[30]="193.136.138.142";
+    int reg_UDP=59000;
+    if (argc!=3 || argc!=5){
+        sprintf()
+    }
 
 
 
